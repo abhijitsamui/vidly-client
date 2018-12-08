12a476967bec5c8ab2174c0b64d4c6a19b9076e8
Abhijit Samui <abhijit.samui@gmail.com>
2018-12-07 17:45:26 -0700

    adding proptypes validation

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
 
 const Pagination = props => {
@@ -26,4 +27,10 @@ const Pagination = props => {
   );
 };
 
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
+};
 export default Pagination;
