d346824fa92090129780705806f1373028da7a1c
Abhijit Samui <abhijit.samui@gmail.com>
2018-12-09 20:53:03 -0700

    Adding Basic Input Validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,9 +2,27 @@ import React, { Component } from "react";
 import Input from "./common/input";
 
 class LoginForm extends Component {
-  state = { account: { username: "", password: "" } };
+  state = {
+    account: { username: "", password: "" },
+    errors: {}
+  };
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
   handleSubmit = e => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
 
     console.log("submitted");
   };
@@ -14,7 +32,7 @@ class LoginForm extends Component {
     this.setState({ account });
   };
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -24,12 +42,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
